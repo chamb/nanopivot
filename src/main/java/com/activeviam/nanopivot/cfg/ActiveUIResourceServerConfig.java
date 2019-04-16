a1a8b0be24289791b105e9474689e1e8d55b4b17
Antoine Chambille <ach@activeviam.com>
2019-04-16 10:04:04 +0200

    upgrade to ActivePivot 5.7.8 and ActiveUI 4.2.10

--- src/main/java/com/activeviam/nanopivot/cfg/ActiveUIResourceServerConfig.java
+++ src/main/java/com/activeviam/nanopivot/cfg/ActiveUIResourceServerConfig.java
@@ -1,5 +1,5 @@
 /*
- * (C) ActiveViam 2018
+ * (C) Quartet FS 2016
  * ALL RIGHTS RESERVED. This material is the CONFIDENTIAL and PROPRIETARY
  * property of Quartet Financial Systems Limited. Any unauthorized use,
  * reproduction or transfer of this material is strictly prohibited
@@ -8,13 +8,14 @@ package com.activeviam.nanopivot.cfg;
 
 import java.util.Set;
 
-import org.springframework.context.annotation.Configuration;
-
 import com.qfs.server.cfg.impl.ASpringResourceServerConfig;
 import com.qfs.util.impl.QfsArrays;
+import org.springframework.context.annotation.Configuration;
 
 /**
- * @author ActiveViam
+ * Spring configuration for ActiveUI web application
+ *
+ * @author Quartet FS
  *
  */
 @Configuration
@@ -23,9 +24,16 @@ public class ActiveUIResourceServerConfig extends ASpringResourceServerConfig {
 	/** The namespace of the ActiveUI web application */
 	public static final String NAMESPACE = "ui";
 
-	/** Constructor	 */
+	/** Constructor */
 	public ActiveUIResourceServerConfig() {
 		super("/" + NAMESPACE);
+	}
+
+	@Override
+	protected void registerRedirections(final ResourceRegistry registry) {
+		super.registerRedirections(registry);
+		// Redirect from the root to ActiveUI
+		registry.redirectTo(NAMESPACE + "/index.html", "/");
 	}
 
 	/**
@@ -39,19 +47,18 @@ public class ActiveUIResourceServerConfig extends ASpringResourceServerConfig {
 
 		// ActiveUI web app also serves request to the root, so that the redirection from root to ActiveUI works
 		registry.serve("/")
-				.addResourceLocations("/", "classpath:META-INF/resources/")
+				.addResourceLocations(
+						"/",
+						"classpath:META-INF/resources/")
 				.setCacheControl(getDefaultCacheControl());
 	}
 
+	/**
+	 * Gets the extensions of files to serve.
+	 * @return all files extensions
+	 */
 	@Override
-	protected void registerRedirections(final ResourceRegistry registry) {
-		super.registerRedirections(registry);
-		// Redirect from the root to ActiveUI
-		registry.redirectTo(NAMESPACE + "/index.html", "/");
-	}
-
-	@Override
-	protected Set<String> getServedExtensions() {
+	public Set<String> getServedExtensions() {
 		return QfsArrays.mutableSet(
 				// Default HTML files
 				"html", "js", "css", "map", "json",
@@ -63,20 +70,18 @@ public class ActiveUIResourceServerConfig extends ASpringResourceServerConfig {
 	}
 
 	@Override
-	protected Set<String> getServedDirectories() {
+	public Set<String> getServedDirectories() {
 		return QfsArrays.mutableSet("/");
 	}
 
 	@Override
-	protected Set<String> getResourceLocations() {
+	public Set<String> getResourceLocations() {
 		// ActiveUI is integrated in the sandbox project thanks to Maven integration.
-		// You can read more about this feature here https://support.activeviam.com/documentation/activeui/4.2.2-rc/dev/setup/maven-integration.html
+		// You can read more about this feature here https://support.activeviam.com/documentation/activeui/4.2.0/dev/setup/maven-integration.html
 
 		return QfsArrays.mutableSet(
 				"/activeui/", // index.html, favicon.ico, etc.
-				"classpath:META-INF/resources/activeviam/activeui-sdk/", // ActiveUI SDK UMD scripts and supporting assets
-				"classpath:META-INF/resources/webjars/react/16.3.1/umd/",
-				"classpath:META-INF/resources/webjars/react-dom/16.3.1/umd/");
+				"classpath:META-INF/resources/webjars/activeui/"); // ActiveUI SDK UMD scripts and supporting assets
 	}
 
 }
